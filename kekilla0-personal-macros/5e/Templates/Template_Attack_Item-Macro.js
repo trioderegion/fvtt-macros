0c265b5eee12dbc8ca3cf574f0d09720cd1323b8
trioderegion <matt.haentschke@gmail.com>
2020-09-29 15:28:51 -0400

    pulled in kekilla0's macros

---
+++ kekilla0-personal-macros/5e/Templates/Template_Attack_Item-Macro.js
@@ -0,0 +1,12 @@
+(async () => {
+  if(game.user.target.size !== 1) return ui.notificiations.error(`Target 1 Token`);
+  let target_actor = Array.from(game.user.targets)[0].actor;
+
+  let attack_roll = await item.rollAttack();
+  if(attack_roll.total >= target_actor.data.data.attributes.ac.value)
+  {
+    let damage_roll = await item.rollDamage();
+  }
+})();
+
+
