69e65ef9b7b46ea412bf129d600e72076d2fbe8b
trioderegion <matt.haentschke@gmail.com>
2020-10-06 11:24:03 -0400

    removed extra logging

--- honeybader-macros/AutoWildMagicSurge.js
+++ honeybader-macros/AutoWildMagicSurge.js
@@ -6,7 +6,6 @@ Hooks.on("preUpdateActor", async (actor, update, options, userId) => {
   /** find the spell level just cast */
   const spellLvlNames = ["spell1","spell2","spell3","spell4","spell5","spell6", "spell7","spell8","spell9"];
   let lvl = spellLvlNames.findIndex(name => { return getProperty(update,"data.spells."+name) });
-  console.log("Level "+ (lvl+1) +"spell detected");
 
   const preCastSlotCount = getProperty(origSlots, spellLvlNames[lvl]+".value");
   const postCastSlotCount = getProperty(update, "data.spells."+spellLvlNames[lvl]+".value");
@@ -17,7 +16,7 @@ Hooks.on("preUpdateActor", async (actor, update, options, userId) => {
   const wmFeature = actor.items.find(i => i.name === "Wild Magic: Wild Magic Surge") !== null
 
   lvl++;
-  console.log("A "+lvl+" slot was expended("+bWasCast+") by a user with the Wild Magic Feature("+wmFeature+")");
+  console.log("A level "+lvl+" slot was expended("+bWasCast+") by a user with the Wild Magic Feature("+wmFeature+")");
   if(wmFeature && bWasCast && lvl>0)
   {
       /** lets go baby lets go */
