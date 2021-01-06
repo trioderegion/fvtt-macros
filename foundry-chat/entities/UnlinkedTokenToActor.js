78cb6fea605e7390e59af23e8d4495a7a72bb315
trioderegion <matt.haentschke@gmail.com>
2021-01-06 16:57:32 -0500

    added kekilla's unlinked token to actor

---
+++ foundry-chat/entities/UnlinkedTokenToActor.js
@@ -0,0 +1,16 @@
+//by Kekilla
+
+(async ()=>{
+  if(!token || token.data.actorLink || token.data.actorData === {}) return;
+
+  let {actorData} = duplicate(token.data);
+
+  let baseActor = game.actors.get(token.data.actorId);
+  let newActor = await Actor.create(baseActor.data);
+
+  await newActor.update({
+    "name" : newActor.data.name + "--NEW",
+    "token.actorLink" : true,
+    ...actorData
+  });
+})();
