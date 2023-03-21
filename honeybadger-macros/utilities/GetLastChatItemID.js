afc8c51d41b983441ff3e386edc7e701ae64de80
trioderegion <matt.haentschke@gmail.com>
2023-03-21 15:49:42 -0400

    Create GetLastChatItemID.js

---
+++ honeybadger-macros/utilities/GetLastChatItemID.js
@@ -0,0 +1,12 @@
+let message = game.messages
+    .filter(message => message.content.includes(`dnd5e chat-card item-card`))
+    .pop();
+
+//this will get you the last message that was a dnd5e chat-card
+
+//to do : find a way to parse the string for actor_id/item_id
+let actor_id = /data-actor-id="(.*?)"/g.exec(message.content)[1];
+let item_id = /data-item-id="(.*?)"/g.exec(message.content)[1];
+
+let item = game.actors.get(actor_id).items.get(item_id);
+console.log(item)
