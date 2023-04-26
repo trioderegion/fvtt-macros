179ceb65a58b0614261d1a8ac9deb0d7857d1e68
trioderegion <matt.haentschke@gmail.com>
2023-04-26 10:39:50 -0400

    Update AnimationWait.js

--- honeybadger-macros/utilities/AnimationWait.js
+++ honeybadger-macros/utilities/AnimationWait.js
@@ -1,4 +1,4 @@
 await token.document.update({x:300,y:300});
 ui.notifications.info("Starting...");
-await CanvasAnimation.getAnimation(token.movementAnimationName)?.promise
+await CanvasAnimation.getAnimation(token.animationName)?.promise
 ui.notifications.info("Done")
